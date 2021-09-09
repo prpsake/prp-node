0e2e08318d8cdff0a1d576a58c6d4f733ef0c968
gourmetseasoningsake <jerome@shtill.com>
2021-09-10 00:03:02 +0200

    add PDF tests

---
+++ src/cmd/PDF.spec.js
@@ -0,0 +1,42 @@
+import test from 'ava'
+import sinon from 'sinon'
+
+
+import { delimiter } from '../Log.bs.js'
+//import * as Validator from '../Validator.bs.js'
+import { PDF } from './PDF.bs.js'
+
+
+
+const removeAnsiCodes =
+  str =>
+  str.replace(/\\(u00|x)1b\[[0-9]{1,2}m/g, '')
+
+
+
+test.serial(
+`#pdf
+  Resolves with an error, if template directory does not exist.`,
+  
+  async t => {
+    //s
+    const path = '💀/💀/💀'
+    const value =
+      JSON.stringify(
+        [ 'PRP' 
+        , 'prp-node pdf'
+        , `failed : ENOENT: no such file or directory, open '${path}/index.html'`
+        ].join(delimiter)
+      )
+
+    //e
+    const errorMsg = await PDF.pdf({ templatedir: path })
+    const [ ctx, _, cmd, msg ] = 
+      removeAnsiCodes(JSON.stringify(errorMsg))
+      .split(delimiter)
+    const result = [ ctx, cmd, msg ].join(delimiter)
+
+    //v
+    t.is(result, value)
+  }
+)
